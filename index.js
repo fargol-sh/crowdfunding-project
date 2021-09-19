5e15f15f7de644cd049409514090601c013fd6d1
Fargol Shirvanifar <fargolsh79@gmail.com>
2021-09-20 00:05:02 +0430

    fixing mobile navigation bug error handling for pledge input changing item numbers after confirming a pledge and a little ui ...

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ let totalBackers = document.querySelector('.statistics__item--total-backers');
 let successModal = document.querySelector('.success-modal');
 let successModalContent = document.querySelector('.success-modal__content');
 let successModalBtn = document.querySelector('.success-modal__btn');
-let navIcon = document.querySelector('.sidebar-icon');
+let navIcon = document.querySelector('.sidebar');
 let closeNavIcon = document.querySelector('.mobile-nav__icon-box');
 let mobileNav = document.querySelector('.mobile-nav');
 let mobileNavContent = document.querySelector('.mobile-nav__content');
+let errorMessage = document.querySelectorAll('.btp-overlay__item--error-message');
+let itemNumbers = document.querySelectorAll('.item-number');
+let btpItemNumbers = document.querySelectorAll('.btp-item-number');
 
 
 const closeBtpHandler = function() {
@@ -32,25 +35,20 @@ const openBtpHandler = function() {
 }
 
 const footerHandler = function() {
-  for(let i = 1; i < radioBtns.length; i++)
+  for(let i = 0; i < radioBtns.length; i++)
   {
-    if(radioBtns[0].checked) {
-      items[0].style.border = '.1px solid hsl(176, 50%, 47%)';
-    } else {
-      items[0].style.border = '.1px solid #CDCDCD';
-    }
     if(radioBtns[i].checked) {
-      itemFooter[i - 1].style.visibility = 'visible';
-      itemFooter[i - 1].style.width = 'auto';
-      itemFooter[i - 1].style.height = 'auto';
-      itemFooter[i - 1].style.transform = 'scale(1)';
+      itemFooter[i].style.visibility = 'visible';
+      itemFooter[i].style.width = 'auto';
+      itemFooter[i].style.height = 'auto';
+      itemFooter[i].style.transform = 'scale(1)';
       items[i].style.border = '.1px solid hsl(176, 50%, 47%)';
 
     } else {
-      itemFooter[i - 1].style.visibility = 'hidden';
-      itemFooter[i - 1].style.width = 0;
-      itemFooter[i - 1].style.height = 0;
-      itemFooter[i - 1].style.transform = 'scale(0)';
+      itemFooter[i].style.visibility = 'hidden';
+      itemFooter[i].style.width = 0;
+      itemFooter[i].style.height = 0;
+      itemFooter[i].style.transform = 'scale(0)';
       items[i].style.border = '.1px solid #CDCDCD';
     }
   }
@@ -69,8 +67,41 @@ const closeSuccessModalHandler = function() {
 }
 
 const footerBtnHandler = function(k) {
+  if(!inputPledge[k].value) {
+    errorMessage[k].innerHTML = 'Please enter a valid number!';
+    inputPledge[k].style.border = '.1px solid red';
+    return;
+  }
+  if (k === 1) {
+    if(parseInt(inputPledge[k].value) < 25) {
+      errorMessage[k].innerHTML = 'Please enter a valid number!';
+      inputPledge[k].style.border = '.1px solid red';
+      return;
+    }
+  } else if (k === 2) {
+    if(parseInt(inputPledge[k].value) < 75) {
+      errorMessage[k].innerHTML = 'Please enter a valid number!';
+      inputPledge[k].style.border = '.1px solid red';
+      return;
+    }
+  } else if (k === 3) {
+    if(parseInt(inputPledge[k].value) < 200) {
+      errorMessage[k].innerHTML = 'Please enter a valid number!';
+      inputPledge[k].style.border = '.1px solid red';
+      return;
+    }
+  }
+  for(let m = 0; m < errorMessage.length; m++) {
+    errorMessage[m].innerHTML = '';
+    inputPledge[m].style.border = '.1px solid #CDCDCD';
+  }
   raisedMoney.innerHTML = '$' + (parseInt(raisedMoney.innerHTML.replace(/[^0-9]/g, '')) + parseInt(inputPledge[k].value)).toString();
   totalBackers.innerHTML = (parseInt(totalBackers.innerHTML.replace(/[^0-9]/g, '')) + 1).toString();
+
+  if(k !== 0) {
+    itemNumbers[k - 1].innerHTML = (parseInt(itemNumbers[k - 1].innerHTML) - 1).toString();
+    btpItemNumbers[k].innerHTML = (parseInt(btpItemNumbers[k].innerHTML) - 1).toString();
+  }
   closeBtpHandler();
   setTimeout(() => {
     for(let n = 0; n < inputPledge.length; n++) {
@@ -85,21 +116,29 @@ const footerBtnHandler = function(k) {
 }
 
 const openNavHandler = function() {
+  navIcon.style.transform = 'scale(0)';
+  navIcon.style.display = 'none';
   mobileNav.style.opacity = 1;
   mobileNav.style.visibility = 'visible';
   mobileNavContent.style.opacity = 1;
-  // mobileNavContent.style.transform = 'scale(1)';
-  navIcon.style.display = 'none';
-  // closeNavIcon.style.visibility = 'visible';
+  mobileNavContent.style.transform = 'scale(1)';
+  setTimeout(() => {
+    closeNavIcon.style.visibility = 'visible';
+    closeNavIcon.style.transform = 'scale(1)';
+  }, 300)
 }
 
 const closeNavHandler = function() {
   mobileNav.style.opacity = 0;
   mobileNav.style.visibility = 'hidden';
   mobileNavContent.style.opacity = 0;
-  // mobileNavContent.style.transform = 'scale(.95)';
-  navIcon.style.display = 'block';
-  // closeNavIcon.style.visibility = 'hidden';
+  mobileNavContent.style.transform = 'scale(.95)';
+  closeNavIcon.style.visibility = 'hidden';
+  closeNavIcon.style.transform = 'scale(0)';
+  setTimeout(() => {
+    navIcon.style.display = 'block';
+    navIcon.style.transform = 'scale(1)';
+  }, 400);
 }
 
 document.querySelector('.cta__back-this-project').addEventListener('click', openBtpHandler);
@@ -119,6 +158,8 @@ successModalBtn.addEventListener('click', closeSuccessModalHandler);
 
 navIcon.addEventListener('click', openNavHandler);
 closeNavIcon.addEventListener('click', closeNavHandler);
+
+
 
 
 
